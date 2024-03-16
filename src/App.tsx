eb4a0922a8dcac989cad891be4633dc6803e3a9f
jumang4423 <catontheskate4423@gmail.com>
2024-03-17 03:31:06 +0900

    lkajsdflkjsdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import useSound from "use-sound";
 import "./App.css";
 import "./font.css";
-
+import hov_1 from "/hov_1.mp3";
+import hov_2 from "/hov_2.mp3";
+import hov_3 from "/hov_3.mp3";
 const headerVideo = "/logo_b.mp4";
 
 function Header() {
@@ -12,7 +15,8 @@ function Header() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        marginTop: "-5px",
+        marginTop: "-8px",
+        marginBottom: "-8px",
       }}
     >
       <motion.div
@@ -29,57 +33,47 @@ function Header() {
           loop
           playsInline
           muted
-          width={520}
-          height={200}
+          width={500}
           style={{
             objectFit: "cover",
           }}
         />
       </motion.div>
+      <div
+        className="header-smartphone-container"
+        style={{
+          marginTop: "8px",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src="/juma.png"
+          style={{
+            objectFit: "cover",
+            width: "112%",
+          }}
+        />
+      </div>
     </motion.div>
   );
 }
 
-const initLine = ["🌈", "✨", "🎵", "🌱", "💿", "🎹", "🎶", "🎧", "🎤", "🐶"];
-const initLines = [...initLine, ...initLine];
-function RainbowLine() {
-  const [lines, setLines] = useState<Array<string>>(initLines);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // shaffle 12 times
-      setLines((lines) => {
-        for (let i = 0; i < 3; i++) {
-          const a = Math.floor(Math.random() * lines.length);
-          const b = Math.floor(Math.random() * lines.length);
-          const tmp = lines[a];
-          lines[a] = lines[b];
-          lines[b] = tmp;
-        }
-        return [...lines];
-      });
-    }, 100);
-
-    return () => clearInterval(interval);
-  });
-
+function Link({
+  children,
+  href,
+  onPlay,
+}: {
+  children: any;
+  href: string;
+  onPlay: () => void;
+}) {
   return (
-    <div
-      style={{
-        marginBottom: "16px",
-        marginTop: "32px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {lines}
-    </div>
-  );
-}
-
-function Link({ children, href }: { children: any; href: string }) {
-  return (
-    <a
+    <motion.a
+      initial={{ scale: 1.0 }}
+      whileHover={{ scale: 1.25 }}
       href={href}
       target="_blank"
       rel="noreferrer"
@@ -88,30 +82,16 @@ function Link({ children, href }: { children: any; href: string }) {
         backgroundColor: "rgb(255, 164, 244)",
         color: "white",
         textDecoration: "underline",
+        fontWeight: "normal",
+      }}
+      onClick={() => {
+        onPlay();
       }}
     >
       {children}
-    </a>
+    </motion.a>
   );
 }
-
-interface Emotions {
-  [key: string]: {
-    backgroundColor: string;
-    color: string;
-  };
-}
-
-const emotions: Emotions = {
-  generic: {
-    backgroundColor: "rgb(255, 164, 244)",
-    color: "white",
-  },
-  futuristic: {
-    backgroundColor: "skyblue",
-    color: "white",
-  },
-};
 
 function Subtitle({ title }: { title: string }) {
   return (
@@ -134,10 +114,12 @@ function SoundCloudLinkCard({
   imgSrc,
   href,
   title,
+  onPlay,
 }: {
   imgSrc?: string;
   href: string;
   title: string;
+  onPlay: () => void;
 }) {
   return (
     <div
@@ -148,64 +130,182 @@ function SoundCloudLinkCard({
       }}
     >
       <img src={imgSrc} width={32} height={32} />
-      <Link href={href}>{title}</Link>
+      <Link href={href} onPlay={onPlay}>
+        {title}
+      </Link>
     </div>
   );
 }
 
 function Description() {
-  // left sided text
+  const volume = 0.5;
+  const [playbackRate, setPlaybackRate] = useState(1.0);
+  const [play1] = useSound(hov_1, {
+    volume,
+    playbackRate,
+  });
+  const [play2] = useSound(hov_2, {
+    volume,
+    playbackRate,
+  });
+  const [play3] = useSound(hov_3, {
+    volume,
+    playbackRate,
+  });
+  const plays = [play1, play2, play3];
+  const onPlay = () => {
+    const randomPlay = plays[Math.floor(Math.random() * plays.length)];
+    randomPlay();
+    const randomRate = Math.random() * 3.0 + 0.1;
+    setPlaybackRate(randomRate);
+  };
   return (
-    <div style={{}}>
+    <div
+      style={{
+        marginLeft: "16px",
+        marginRight: "16px",
+      }}
+    >
       <Subtitle title="about" />
       <div>
-        jumango(@jumang4423) is a bedroom producer and haxOr from japan.
+        jumango(@jumang4423) is a musician and hacker in Japan, making
+        futuristic d/acc music and software
       </div>
-      <div>i make futuristic e/acc music and software.</div>
-      <Subtitle title="patreon/donation" />
+      <div>
+        regularly i experiment with new technologies and find ways to understand
+        gen alpha
+      </div>
       <div>
         <SoundCloudLinkCard
           href="https://etherscan.io/address/0x4bd46fe39b1630915687b4edd3de582c85c20bf9"
-          title="0x4bd46fe39b1630915687b4edd3de582c85c20bf9"
+          title="0x4bd46fe3....85c20bf9"
           imgSrc="/ether.svg"
+          onPlay={onPlay}
         />
-        <div>patreon is coming soon.</div>
       </div>
-      <Subtitle title="find me" />
-      <div>
+      <Subtitle title="sns" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <div>jumango's work is most audible via:</div>
-        <Link href="https://soundcloud.com/jumang4423">SoundCloud</Link>,
-        <Link href="https://jumango.bandcamp.com/">Bandcamp</Link>,
-        <Link href="https://vrchat.com/home/user/usr_4b57f0ea-9bb7-4a9a-9f6c-42b3734c9ee3">
-          VRChat
-        </Link>{" "}
-        or
-        <Link href="https://twitter.com/jumang4423">Twitter</Link>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            gap: "8px 0px",
+          }}
+        >
+          <Link href="https://soundcloud.com/jumang4423/" onPlay={onPlay}>
+            soundcloud
+          </Link>
+          ,
+          <Link onPlay={onPlay} href="https://jumango.bandcamp.com/">
+            bandcamp
+          </Link>
+          ,
+          <Link onPlay={onPlay} href="https://github.com/jumang4423/">
+            github
+          </Link>
+          ,
+          <Link
+            onPlay={onPlay}
+            href="https://vrchat.com/home/user/usr_4b57f0ea-9bb7-4a9a-9f6c-42b3734c9ee3/"
+          >
+            vrchat
+          </Link>
+          <pre style={{ margin: "0px" }}>{" or"}</pre>
+          <Link onPlay={onPlay} href="https://twitter.com/jumang4423/">
+            x(twitter)
+          </Link>
+        </div>
       </div>
       <Subtitle title="releases" />
-      <SoundCloudLinkCard
-        href="https://soundcloud.com/jumang4423/twinkle"
-        title="twinkle"
-        imgSrc="https://i1.sndcdn.com/artworks-EPu65YpZOrOimCxH-xKyKhw-t500x500.jpg"
-      />
-      <SoundCloudLinkCard
-        href="https://soundcloud.com/jumang4423/frail"
-        title="frail"
-        imgSrc="https://i1.sndcdn.com/artworks-dzLoHPQgjDSU2a1m-2yoiIg-t500x500.jpg"
-      />
-      <SoundCloudLinkCard
-        href="https://soundcloud.com/jumang4423/trip"
-        title="trip"
-        imgSrc="https://i1.sndcdn.com/artworks-8Tz5URmo6iE8y0PL-JRLk4A-t500x500.jpg"
-      />
-      <SoundCloudLinkCard
-        href="https://soundcloud.com/jumang4423/blanket"
-        title="blanket"
-        imgSrc="https://i1.sndcdn.com/artworks-1byv2IPH6K2T7CJC-PYdCpQ-t500x500.jpg"
-      />
-
-      <Subtitle title="tags" />
-      <div>#jumang4423 #futuristic #cute</div>
+      available on soundcloud:
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          alignItems: "center",
+          gap: "0px 8px",
+        }}
+      >
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/2na"
+          title="tuna"
+          onPlay={onPlay}
+          imgSrc="https://i1.sndcdn.com/artworks-TXubNLB7Erl1yJqy-DtAWSA-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/lazydoll-away-jumango-remix"
+          title="lazydoll away (remix)"
+          onPlay={onPlay}
+          imgSrc="https://i1.sndcdn.com/artworks-1PB8WApUghwg1BUx-V0V0DA-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/twinkle"
+          onPlay={onPlay}
+          title="twinkle"
+          imgSrc="https://i1.sndcdn.com/artworks-EPu65YpZOrOimCxH-xKyKhw-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/frail"
+          onPlay={onPlay}
+          title="frail"
+          imgSrc="https://i1.sndcdn.com/artworks-dzLoHPQgjDSU2a1m-2yoiIg-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/trip"
+          title="trip"
+          imgSrc="https://i1.sndcdn.com/artworks-8Tz5URmo6iE8y0PL-JRLk4A-t500x500.jpg"
+          onPlay={onPlay}
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/blanket"
+          onPlay={onPlay}
+          title="blanket"
+          imgSrc="https://i1.sndcdn.com/artworks-1byv2IPH6K2T7CJC-PYdCpQ-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/loop"
+          onPlay={onPlay}
+          title="loop"
+          imgSrc="https://i1.sndcdn.com/artworks-1PB8WApUghwg1BUx-V0V0DA-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/me-by-jumango"
+          onPlay={onPlay}
+          title="me"
+          imgSrc="https://i1.sndcdn.com/artworks-1PB8WApUghwg1BUx-V0V0DA-t500x500.jpg"
+        />
+        <SoundCloudLinkCard
+          href="https://soundcloud.com/jumang4423/finally"
+          onPlay={onPlay}
+          title="finally"
+          imgSrc="https://i1.sndcdn.com/artworks-1PB8WApUghwg1BUx-V0V0DA-t500x500.jpg"
+        />
+      </div>
+      <Subtitle title="pinterest" />
+      <div
+        style={{
+          marginTop: "8px",
+        }}
+      >
+        <script
+          type="text/javascript"
+          async
+          defer
+          src="//assets.pinterest.com/js/pinit.js"
+        />
+        <a
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          data-pin-do="embedUser"
+          href="https://www.pinterest.com/jumang4423/"
+        ></a>
+      </div>
     </div>
   );
 }
@@ -225,11 +325,15 @@ function App() {
   return (
     <div
       style={{
-        fontFamily: "Iosevka Web, Transparent",
+        fontFamily: "Iosevka Aile Iaso, Transparent",
         maxWidth: "500px",
         fontSize: "18px",
       }}
     >
+      <link
+        rel="stylesheet"
+        href="https://cdn.xeiaso.net/static/css/iosevka/family.css"
+      />
       <Header />
       <Description />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import AnimatedCursor from "react-animated-cursor";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <AnimatedCursor
-      innerSize={16}
-      outerSize={16}
-      color="255, 164, 244"
-      outerAlpha={0.2}
-      innerScale={0.7}
-      outerScale={5}
-    />
-  </>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
